7a5bde4b6dc7debd8a03ae03a261c553960d9d56
Joao <joaogoya@gmail.com>
2024-04-08 08:59:26 -0300

    Next-gen desabilitado e paleta de cores

--- themes/pipeline/includes/UI-Kit/colors.php
+++ themes/pipeline/includes/UI-Kit/colors.php
@@ -5,25 +5,68 @@
                     <h1>Paleta de cores</h1>
                     <hr>
                 </div>
-            </div>            
+            </div>    
+            <br><br>  
             <div class="row">
+            <h2 class="col-12">Cores do Tema</h2>
+                <div class="col-md-4 color-scheme text-center bg-primaryLight text-dark">
+                    Primary Light
+                </div>
+                <div class="col-md-4 color-scheme text-center bg-primary text-info">
+                    PRIMARY
+                </div>
+                <div class="col-md-4 color-scheme text-center bg-primaryDark text-light">
+                    Primary Dark
+                </div>
+            </div>
+            <br><br>
+            <div class="row">
+                <div class="col-md-4 color-scheme text-center bg-secondaryLight text-info">
+                    Secondary Light
+                </div>
+                <div class="col-md-4 color-scheme text-center bg-secondary text-info">
+                    SECONDARY
+                </div>
+                <div class="col-md-4 color-scheme text-center bg-secondaryDark text-light">
+                    Secondary Daek
+                </div>
+            </div>
+            <br><br>
+            <div class="row">
+                <h2 class="col-12">Comunicação com o usuário</h2>
+                <div class="col-md-3 color-scheme text-center bg-info ">
+                    INFO
+                </div>
+                <div class="col-md-3 color-scheme text-center bg-success text-info">
+                    SUCCESS
+                </div>
+                <div class="col-md-3 color-scheme text-center bg-warning text-dark">
+                    WARNING
+                </div>
+                <div class="col-md-3 color-scheme text-center bg-danger text-info">
+                    DANGER
+                </div>
+            </div>
+            <br><br>    
+            <div class="row">
+                <h2 class="col-12">Escala de cinza</h2>
                 <div class="col-md-6 color-scheme text-center bg-light">
                     LIGHT
                 </div>
-                <div class="col-md-6 color-scheme text-center bg-dark">
+                <div class="col-md-6 color-scheme text-center bg-dark text-info">
                     DARK
                 </div>
             </div>
             <br><br>
             <div class="row">
                 <div class="col-1"></div>
-                <div class="col-md-2 color-scheme text-center bg-extraDarkGray">
+                <div class="col-md-2 color-scheme text-center bg-extraDarkGray text-info">
                     extraDarkGray
                 </div>
-                <div class="col-md-2 color-scheme text-center bg-darkGray">
+                <div class="col-md-2 color-scheme text-center bg-darkGray text-info">
                     darkGray
                 </div>
-                <div class="col-md-2 color-scheme text-center bg-gray">
+                <div class="col-md-2 color-scheme text-center bg-gray text-info">
                     gray
                 </div>
                 <div class="col-md-2 color-scheme text-center bg-lightGray">
@@ -34,28 +77,8 @@
                 </div>
                 <div class="col-1"></div>
             </div>
-            <br><br>
-            <div class="row">
-                <div class="col-md-2 color-scheme text-center bg-primary ">
-                    PRIMARY
-                </div>
-                <div class="col-md-2 color-scheme text-center bg-secondary ">
-                    SECONDARY
-                </div>
-                <div class="col-md-2 color-scheme text-center bg-info ">
-                    INFO
-                </div>
-
-                <div class="col-md-2 color-scheme text-center bg-success ">
-                    SUCCESS
-                </div>
-                <div class="col-md-2 color-scheme text-center bg-warning ">
-                    WARNING
-                </div>
-                <div class="col-md-2 color-scheme text-center bg-danger ">
-                    DANGER
-                </div>
-            </div>
+        
+           
             
         </div>
     </section>
@@ -116,6 +139,21 @@
                 </div>
                 <div class="col-md-2 color-scheme text-center outline-danger">
                     DANGER
+                </div>
+            </div>
+            <BR><BR></BR></BR>
+            <div class="row">
+                <div class="col-md-3 color-scheme text-center outline-primaryLight">
+                    PRIMARY LIGHT
+                </div>
+                <div class="col-md-3 color-scheme text-center outline-primaryDark">
+                    ORIMARY DARK
+                </div>
+                <div class="col-md-3 color-scheme text-center outline-secondaryLight">
+                    SECONDARY LIGHT
+                </div>
+                <div class="col-md-3 color-scheme text-center outline-secondaryDark">
+                    SECONDARY DARK
                 </div>
             </div>
             

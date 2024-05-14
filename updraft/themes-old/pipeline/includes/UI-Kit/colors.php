16d92e99e874031ad5034195f2c3828c468db31f
Joao <joaogoya@gmail.com>
2024-05-14 08:45:36 -0300

    UI kit e alguns plugins

---
+++ updraft/themes-old/pipeline/includes/UI-Kit/colors.php
@@ -0,0 +1,161 @@
+    <section id="cores" class="guideline">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1>Paleta de cores</h1>
+                    <hr>
+                </div>
+            </div>    
+            <br><br>  
+            <div class="row">
+            <h2 class="col-12">Cores do Tema</h2>
+                <div class="col-md-4 color-scheme text-center bg-primaryLight text-dark">
+                    Primary Light
+                </div>
+                <div class="col-md-4 color-scheme text-center bg-primary text-info">
+                    PRIMARY
+                </div>
+                <div class="col-md-4 color-scheme text-center bg-primaryDark text-light">
+                    Primary Dark
+                </div>
+            </div>
+            <br><br>
+            <div class="row">
+                <div class="col-md-4 color-scheme text-center bg-secondaryLight text-info">
+                    Secondary Light
+                </div>
+                <div class="col-md-4 color-scheme text-center bg-secondary text-info">
+                    SECONDARY
+                </div>
+                <div class="col-md-4 color-scheme text-center bg-secondaryDark text-light">
+                    Secondary Daek
+                </div>
+            </div>
+            <br><br>
+            <div class="row">
+                <h2 class="col-12">Comunicação com o usuário</h2>
+                <div class="col-md-3 color-scheme text-center bg-info ">
+                    INFO
+                </div>
+                <div class="col-md-3 color-scheme text-center bg-success text-info">
+                    SUCCESS
+                </div>
+                <div class="col-md-3 color-scheme text-center bg-warning text-dark">
+                    WARNING
+                </div>
+                <div class="col-md-3 color-scheme text-center bg-danger text-info">
+                    DANGER
+                </div>
+            </div>
+            <br><br>    
+            <div class="row">
+                <h2 class="col-12">Escala de cinza</h2>
+                <div class="col-md-6 color-scheme text-center bg-light">
+                    LIGHT
+                </div>
+                <div class="col-md-6 color-scheme text-center bg-dark text-info">
+                    DARK
+                </div>
+            </div>
+            <br><br>
+            <div class="row">
+                <div class="col-1"></div>
+                <div class="col-md-2 color-scheme text-center bg-extraDarkGray text-info">
+                    extraDarkGray
+                </div>
+                <div class="col-md-2 color-scheme text-center bg-darkGray text-info">
+                    darkGray
+                </div>
+                <div class="col-md-2 color-scheme text-center bg-gray text-info">
+                    gray
+                </div>
+                <div class="col-md-2 color-scheme text-center bg-lightGray">
+                    lightGray
+                </div>
+                <div class="col-md-2 color-scheme text-center bg-extraLightGray">
+                    extraLightGray
+                </div>
+                <div class="col-1"></div>
+            </div>
+        
+           
+            
+        </div>
+    </section>
+    <section id="cores" class="guideline">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1>Outlines</h1>
+                    <hr>
+                </div>
+            </div>            
+            <div class="row">
+                <div class="col-md-6 color-scheme text-center outline-light">
+                    <!-- fundo transparente e texto branco -->
+                    LIGHT
+                </div>
+                <div class="col-md-6 color-scheme text-center outline-dark">
+                    DARK
+                </div>
+            </div>
+            <br><br>
+            <div class="row">
+                <div class="col-1"></div>
+                <div class="col-md-2 color-scheme text-center outline-extraDarkGray">
+                    extraDarkGray
+                </div>
+                <div class="col-md-2 color-scheme text-center outline-darkGray">
+                    darkGray
+                </div>
+                <div class="col-md-2 color-scheme text-center outline-gray">
+                    gray
+                </div>
+                <div class="col-md-2 color-scheme text-center outline-lightGray">
+                    lightGray
+                </div>
+                <div class="col-md-2 color-scheme text-center outline-extraLightGray">
+                    extraLightGray
+                </div>
+                <div class="col-1"></div>
+            </div>
+            <br><br>
+            <div class="row">
+                <div class="col-md-2 color-scheme text-center outline-primary">
+                    PRIMARY
+                </div>
+                <div class="col-md-2 color-scheme text-center outline-secondary">
+                    SECONDARY
+                </div>
+                <div class="col-md-2 color-scheme text-center outline-info">
+                    INFO
+                </div>
+
+                <div class="col-md-2 color-scheme text-center outline-success">
+                    SUCCESS
+                </div>
+                <div class="col-md-2 color-scheme text-center outline-warning">
+                    WARNING
+                </div>
+                <div class="col-md-2 color-scheme text-center outline-danger">
+                    DANGER
+                </div>
+            </div>
+            <BR><BR></BR></BR>
+            <div class="row">
+                <div class="col-md-3 color-scheme text-center outline-primaryLight">
+                    PRIMARY LIGHT
+                </div>
+                <div class="col-md-3 color-scheme text-center outline-primaryDark">
+                    ORIMARY DARK
+                </div>
+                <div class="col-md-3 color-scheme text-center outline-secondaryLight">
+                    SECONDARY LIGHT
+                </div>
+                <div class="col-md-3 color-scheme text-center outline-secondaryDark">
+                    SECONDARY DARK
+                </div>
+            </div>
+            
+        </div>
+    </section>
